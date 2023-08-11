8d16156bf6d12ec8c1f88d726c3da719d6c65947
Vladzislav Khurs <130601125+VladKhurs@users.noreply.github.com>
2023-08-11 19:29:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let ans = ''
+    for (let i = 0; i <= (String(n).length + 1); i++) {
+        ans += n % 10
+        n = Math.floor(n / 10)
+    }
+    ans = Number(ans)
+    return ans
 }
